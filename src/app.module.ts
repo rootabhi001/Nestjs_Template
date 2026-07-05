c0ecb8b8c12f4591a3f8baef17c4e7ba785698a9
Abhishek <satheabhishek1@gmail.com>
2026-07-05 22:05:07 +0530

    feat: swagger added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.development',
     }),
+    UserModule,
    
   ],
   controllers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Prisma API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    // Uncomment these when you add JWT authentication
+    // .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
